4543ad498ea794cfb4d272b558bc43388359a815
winterssy <mysu1214@gmail.com>
2019-09-13 22:45:42 +0800

    Get download links in bulk

--- tencent/extract.go
+++ tencent/extract.go
@@ -2,8 +2,9 @@ package tencent
 
 import (
 	"fmt"
-
 	"github.com/winterssy/easylog"
+	"regexp"
+
 	"github.com/winterssy/music-get/common"
 )
 
@@ -12,16 +13,42 @@ const (
 )
 
 func ExtractMP3List(songs []Song, savePath string) ([]*common.MP3, error) {
+	n := len(songs)
+	mids := make([]string, 0, n)
+	for _, i := range songs {
+		mids = append(mids, i.Mid)
+	}
+
 	guid := "7332953645"
+	req := NewSongURLRequest(guid, mids...)
+	if err := req.Do(); err != nil {
+		return nil, err
+	}
+
+	var defaultVkey string
+	re := regexp.MustCompile("vkey=(\\w+)")
+	matched, ok := re.FindStringSubmatch(req.Response.Req0.Data.TestFile2g), re.MatchString(req.Response.Req0.Data.TestFile2g)
+	if ok {
+		defaultVkey = matched[1]
+	}
+
+	urlMap := make(map[string]string, n)
+	for _, i := range req.Response.Req0.Data.MidUrlInfo {
+		if i.Vkey == "" {
+			urlMap[i.SongMid] = defaultVkey
+		} else {
+			urlMap[i.SongMid] = i.Vkey
+		}
+	}
+
 	mp3List := make([]*common.MP3, 0, len(songs))
 	for _, i := range songs {
 		mp3 := i.Extract()
-		vkey, err := getVkey(guid, i.Mid)
-		if err != nil {
+		if urlMap[i.Mid] == "" {
 			easylog.Errorf("get vkey failed: %s", i.Mid)
 			continue
 		}
-		mp3.DownloadURL = fmt.Sprintf(SongDownloadURL, i.Mid, guid, vkey)
+		mp3.DownloadURL = fmt.Sprintf(SongDownloadURL, i.Mid, guid, urlMap[i.Mid])
 		mp3.SavePath = savePath
 		mp3List = append(mp3List, mp3)
 	}
